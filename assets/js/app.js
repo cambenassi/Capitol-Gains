111df3e2cd3b8c0693796e3cf3ae930cba1d365c
cambenassi <cambenassi@gmail.com>
2022-03-24 14:55:33 -0400

    removed space again

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const Trade = require('../models/blog'); 
+const Trade = require('../models/blog');
 
 // register view engine
 app.set('view engine', 'ejs');
