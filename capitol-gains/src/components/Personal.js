41539b68f88d399964d07c096c354e15043381fc
Joshua119 <jmsulliv@DESKTOP-I1CPABR.localdomain>
2022-03-27 16:32:33 -0400

    formatting changes

--- capitol-gains/src/components/Personal.js
+++ capitol-gains/src/components/Personal.js
@@ -2,50 +2,53 @@ import React from 'react'
 
 const Personal = ({ personal }) => {
     return (
-        <table className='bio_info'>
+        <div className='full_bio'>
             <img alt="bioimage" src={personal.photo}/>
-            <tr>
-                <td>
-                    <p>Name: {personal.first_name}</p>
-                    <p>{personal.middle_name}</p>
-                    <p>{personal.last_name}</p>
-                </td>
-                <td>
-                    <p className='rightside'>Transactions: {personal.transactions}</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>Date of Birth: {personal.date_of_birth}</p>
-                </td>
-                <td>
-                    <p className='rightside'>Most Recent Trade: {personal.recent_trade}</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>Party: {personal.party}</p>
-                </td>
-                <td>
-                    <p className='rightside'>Total Trade Volume: ${personal.trade_volume}</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>State: {personal.state}</p>
-                </td>
-            </tr>
-            <tr>
-                <td className="chamber">
-                    <p>Chamber: {personal.chamber}</p>
-                </td>
-                <td>
-                    <p className="graphtext">Recent:</p>
-                    <img id="recgraph" alt="recgraph" src={personal.recent_graph}/>
-                </td>
-            </tr>
+            <table className='bio_info'>
+                <tr>
+                    <td>
+                        <p>Name: {personal.first_name}</p>
+                        <p>{personal.middle_name}</p>
+                        <p>{personal.last_name}</p>
+                    </td>
+                    <td>
+                        <p className='rightside'>Transactions: {personal.transactions}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Date of Birth: {personal.date_of_birth}</p>
+                    </td>
+                    <td>
+                        <p className='rightside'>Most Recent Trade: {personal.recent_trade}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Party: {personal.party}</p>
+                    </td>
+                    <td>
+                        <p className='rightside'>Total Trade Volume: ${personal.trade_volume}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>State: {personal.state}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="chamber">
+                        <p>Chamber: {personal.chamber}</p>
+                    </td>
+                    <td>
+                        <p className="graphtext">Recent:</p>
+                    </td>
+                </tr>
 
-        </table>
+            </table>
+
+        </div>
+
     )
 }
 
