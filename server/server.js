34db8597e7265f84b77602f4558a5c471f8ef060
cambenassi <cambenassi@gmail.com>
2022-04-10 11:02:13 -0400

    add routing to build folder for index.html

--- server/server.js
+++ server/server.js
@@ -16,6 +16,17 @@ app.use(express.static('public'));
 // API CODE
 app.use(express.json({ limit: '1mb' }));
 
+//routing to build folder for index.html
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
+
 // API routing to virtual database
 app.post('/api', async (request, response) => {
     console.log('I got a request to test!');
